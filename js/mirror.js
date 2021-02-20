92b1f9f452b52272bef8e0d2a8956cc5d6a78146
BassMio <fredericatoms@gmail.com>
2021-02-20 19:41:30 +0800

    Site updated: 2021-02-20 19:41:30

--- js/mirror.js
+++ js/mirror.js
@@ -1,1 +1,29 @@
-"use strict";function Mirror(){var o,i=window.location.pathname,t=window.location.hostname;"bassmio.cool"===t?(o="https://bassmio.gitee.io"+i,window.alert("即将为您跳转至镜像站"),window.location.href=o):"bassmio.gitee.io"===t?(o="https://bassmio.cool"+i,window.alert("当前为镜像站，即将返回主站"),window.location.href=o):window.alert("本地调试，无需跳转")}
+function Mirror() {
+    let pathname;
+    let hostname;
+    let url;
+    pathname = window.location.pathname;
+    hostname = window.location.hostname;
+    // if (hostname === '域名，不带https://协议')
+    if (hostname === 'bassmio.cool') { //如果是主站
+      // url = "域名，带https://协议" + pathname;
+      url = "https://bassmio.gitee.io" + pathname; //就跳转到镜像站的同名页面
+      window.alert("即将为您跳转至镜像站");
+      window.location.href = url;
+    }
+    else if(hostname === 'bassmio.gitee.io') {
+      url = "https://bassmio.cool" + pathname;
+      window.alert("当前为镜像站，即将返回主站");
+      window.location.href = url;
+    }
+    else {
+      window.alert("本地调试，无需跳转");
+    }
+  }
+
+
+
+
+
+
+  
